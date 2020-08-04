25bfe84fb384cf939106e2200e68b6d4014543f7
in2out <in2out@gmail.com>
2020-08-05 08:28:41 +0900

    fix warnning

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,4 +1,4 @@
-use ui;
+//use ui;
 use net::PacketInfo;
 use pcap::Device;
 use std::io::{self,Write};
@@ -9,13 +9,13 @@ fn main(){
     for _ in 0..5 {
 // ex, 16:55:00.511963 IP SNIPER.ssh > 10.0.80.108.60371: Flags [P.], seq 933769480:933769676, ack 2243359901, win 254, length 196
         let packet = cap.next();
-        let mut pInfo = PacketInfo::new();
+        let mut pkt_info = PacketInfo::new();
         match packet {
             Ok(packet) => {
                 print!("{}.{} ", packet.header.ts.tv_sec, packet.header.ts.tv_usec);
 
-                pInfo = pInfo.parsing(packet.data); 
-                pInfo.print();
+                pkt_info = pkt_info.parsing(packet.data); 
+                pkt_info.print();
 
                 io::stdout().flush().unwrap();
                 println!("");
