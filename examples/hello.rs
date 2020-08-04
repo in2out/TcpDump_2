910680d68fe60ae303bef4bd223da56036c44673
in2out <in2out@gmail.com>
2020-08-04 17:02:26 +0900

    패킷 read하여 시간값과 IP version 표시

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,6 +1,32 @@
 use ui;
-use net;
+use net::PacketInfo;
+use pcap::Device;
+use std::io::{self,Write};
 
 fn main(){
-    println!("hello");
+    let mut cap = Device::lookup().unwrap().open().unwrap();
+//    while let Ok(packet) = cap.next() {
+    for _ in 0..5 {
+// ex, 16:55:00.511963 IP SNIPER.ssh > 10.0.80.108.60371: Flags [P.], seq 933769480:933769676, ack 2243359901, win 254, length 196
+        let packet = cap.next();
+        let mut pInfo = PacketInfo::new();
+        match packet {
+            Ok(packet) => {
+                print!("{}.{} ", packet.header.ts.tv_sec, packet.header.ts.tv_usec);
+
+                pInfo = pInfo.parsing(packet.data); 
+                pInfo.print();
+
+                io::stdout().flush().unwrap();
+                println!("");
+            },
+            Err(e) => {
+                println!("{:?}", e);
+                continue; 
+            }
+        }
+    }
+
+    let stats = cap.stats().unwrap();
+    println!("Received: {}, Dropped: {}, if_dropped: {}", stats.received, stats.dropped, stats.if_dropped);
 }
--- net/src/lib.rs
+++ net/src/lib.rs
@@ -1,3 +1,66 @@
+use core::convert::TryInto;
+//use std::fmt::Debug::fmt;
+
+pub struct PacketInfo {
+    caplen: u32,
+    ip_ver: u32, 
+    protocol: u32,
+    srcip: u32,
+    dstip: u32,
+    sport: u16,
+    dport: u16,
+    tcp_flags: u8,
+    tcp_seq: u32,
+    tcp_ack: u32,
+    win: u32,
+}
+
+pub struct net_ip_hdr {
+    ip_v_hl: u8,      /* version,  header length */
+    ip_tos: u8,       /* type of service */
+    ip_len: u16,       /* total length */
+    ip_id: u16,        /* identification */
+    ip_off: u16,       /* fragment, offset */
+    ip_ttl: u8,      /* time to live */
+    ip_proto: u8,    /* protocol */
+    ip_sum: u16,      /* checksum */
+    ip_src: u32,      /* source ip address */
+    ip_dst: u32,      /* destination ip address */
+}
+
+impl PacketInfo {
+    pub fn new() -> PacketInfo {
+        PacketInfo {
+            caplen: 0,
+            ip_ver: 0,
+            protocol: 0,
+            srcip: 0,
+            dstip: 0,
+            sport: 0,
+            dport: 0,
+            tcp_flags: 0,
+            tcp_seq: 0,
+            tcp_ack: 0,
+            win: 0,
+        }
+    }
+
+    pub fn parsing(mut self, data: &[u8]) -> PacketInfo {
+        let ether = u16::from_be_bytes(data[12..14].try_into().unwrap());
+
+        match ether {
+            0x0800 => self.ip_ver = 4,
+            0x86dd => self.ip_ver = 6,
+            _ => self.ip_ver = 0,
+        }
+        self
+    }
+
+    pub fn print(self) {
+        print!("{} {}.{} > {}.{}, length {}", self.ip_ver, self.srcip, self.sport, self.dstip, self.dport, self.caplen);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
